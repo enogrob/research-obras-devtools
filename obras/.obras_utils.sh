5d4c834aa298757aaf5e95d05617d7b62a9aa1c1
Roberto Nogueira <enogrob@gmail.com>
2020-04-30 06:35:14 -0300

    update-obras

--- obras/.obras_utils.sh
+++ obras/.obras_utils.sh
@@ -343,7 +343,7 @@ db(){
     help|h|--help|-h)
       __pr bold "Crafted (c) 2013~2020 by InMov - Intelligence in Movement"
       __pr bold "::"
-      __pr info "db" "[ls || preptest || drop || create || migrate || seed || import [dbfile] || download"
+      __pr info "db" "[ls || preptest || drop || create || migrate || seed || import [dbfile] || download]"
       __pr info "db" "[status || start || stop || restart || tables || databases || socket]"
       __pr 
       ;; 
